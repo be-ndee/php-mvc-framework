95de359e551c5ef8959e7f92ad499a98c6ca4082
Andreas Bissinger <mail@bissinger-andreas.de>
2015-01-31 15:45:13 +0100

    Exception for config file

---
+++ src/Core/ErrorHandling/ConfigFileException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Core\ErrorHandling;
+use Core\ErrorHandling\Exception;
+
+/**
+ * This exception is used if something went wrong with the config file.
+ * @author Andreas Bissinger <mail@bissinger-andreas.de>
+ */
+class ConfigFileException extends Exception {
+    public function __construct ($fileName) {
+        parent::__construct('The given config file does not exist or is invalid. Filename"' . fileName . '".');
+    }
+}
