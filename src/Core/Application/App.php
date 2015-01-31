8521e2010c1cb6f7108f445ddd9c121f4d4fee80
Andreas Bissinger <mail@bissinger-andreas.de>
2015-01-31 10:59:56 +0100

    Add doAction function in App

--- src/Core/Application/App.php
+++ src/Core/Application/App.php
@@ -1,10 +1,25 @@
 <?php
 
 namespace Core\Application;
+use Core\Routing\Request;
+use Core\Routing\Action;
 
 /**
  * This class represents the whole application. It manages all request and calls the responsible routers.
  * @author Andreas Bissinger <mail@bissinger-andreas.de>
  */
 class App {
+    /**
+     * Do the given action and handle its return value.
+     * @param Action  $action  The action which should be called.
+     * @param Request $request The request which came in.
+     * @return void
+     */
+    public function doAction ($action, $request) {
+        if ($action->actionExists()) {
+            $object = $action->getControllerObject($request);
+            $action = $action->getActionName();
+            $object->$action();
+        }
+    }
 }
