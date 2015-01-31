302e46df11b6da09e7b8b41ea35d0cf05e359173
Andreas Bissinger <mail@bissinger-andreas.de>
2015-01-31 14:42:41 +0100

    Add own Exception class, but has no difference to PHP Exception class

---
+++ src/Core/ErrorHandling/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Core\ErrorHandling;
+
+/**
+ * This class is used for custom exceptions. It offers the possibility to write in log files.
+ * @author Andreas Bissinger <mail@bissinger-andreas.de>
+ */
+class Exception extends \Exception {
+}
